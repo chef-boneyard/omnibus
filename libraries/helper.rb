4d16a34ef7abb3b497f9a88fe549e62b31020fea
Seth Vargo <sethvargo@gmail.com>
2014-03-07 16:08:36 -0500

    Remove unused helper

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -27,41 +27,6 @@ module Omnibus
     def windows_safe_path_expand(arg)
       ::File.expand_path(arg).gsub(::File::SEPARATOR, ::File::ALT_SEPARATOR)
     end
-
-    # Ensures $HOME is temporarily set to the given user. The original
-    # $HOME is preserved and re-set after the block has been yielded
-    # to.
-    #
-    # This is a workaround for CHEF-3940. TL;DR Certain versions of
-    # `git` misbehave if configuration is inaccessible in $HOME.
-    #
-    # More info here:
-    #
-    #   https://github.com/git/git/commit/4698c8feb1bb56497215e0c10003dd046df352fa
-    #
-    def with_home_for_user(username, &block)
-      time = Time.now.to_i
-
-      ruby_block "set HOME for #{username} at #{time}" do
-        block do
-          ENV['OLD_HOME'] = ENV['HOME']
-          ENV['HOME'] = begin
-            require 'etc'
-            Etc.getpwnam(username).dir
-          rescue ArgumentError # user not found
-            "/home/#{username}"
-          end
-        end
-      end
-
-      yield
-
-      ruby_block "unset HOME for #{username} #{time}" do
-        block do
-          ENV['HOME'] = ENV['OLD_HOME']
-        end
-      end
-    end
   end
 end
 
