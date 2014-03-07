4cef2cf2c5b816f67945fcd0737758ab5311d123
Seth Vargo <sethvargo@gmail.com>
2014-03-07 16:46:33 -0500

    Install the latest version of bash (4.3)

--- recipes/_bash.rb
+++ recipes/_bash.rb
@@ -21,11 +21,11 @@ include_recipe 'chef-sugar::default'
 include_recipe 'build-essential::default'
 
 remote_install 'bash' do
-  source 'http://ftp.gnu.org/gnu/bash/bash-4.0.tar.gz'
-  version '4.0'
-  checksum '9793d394f640a95030c77d5ac989724afe196921956db741bcaf141801c50518'
+  source 'http://ftp.gnu.org/gnu/bash/bash-4.3.tar.gz'
+  version '4.3'
+  checksum 'afc687a28e0e24dc21b988fa159ff9dbcf6b7caa92ade8645cc6d5605cd024d4'
   build_command './configure'
   compile_command 'make'
   install_command 'make install'
-  not_if { installed_at_version?('bash', '4.0') }
+  not_if { installed_at_version?('bash', '4.3') }
 end
