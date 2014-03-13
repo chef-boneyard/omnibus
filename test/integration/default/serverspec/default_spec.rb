01ac125f2248f4682d5ad9c2a214331e7eb43d86
Seth Vargo <sethvargo@gmail.com>
2014-03-13 17:11:03 -0400

    Update ServerSpec tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -17,47 +17,31 @@ if RUBY_PLATFORM =~ /darwin/
   end
 end
 
-context 'ccache' do
-  describe file('/usr/local/bin/ccache') do
-    it { should be_executable }
-  end
-
+describe 'ccache' do
   describe command('/usr/local/bin/ccache --version') do
     it { should return_stdout(/3\.1\.9/) }
   end
 
   %w[gcc g++ cc c++].each do |compiler|
     describe file("/usr/local/bin/#{compiler}") do
-      it { should be_linked_to '/usr/local/bin/ccache' }
+      it { should be_linked_to('/usr/local/bin/ccache') }
     end
   end
 end
 
-context 'ruby' do
-  describe command('which ruby') do
-    it { should return_stdout('/bin/ruby') }
-  end
-
+describe 'ruby' do
   describe command('/usr/local/bin/ruby --version') do
     it { should return_stdout(/^ruby 2\.1\.1(.+)/) }
   end
 end
 
-context 'bash' do
-  describe command('which bash') do
-    it { should return_stdout('/bin/bash') }
-  end
-
+describe 'bash' do
   describe command('/usr/local/bin/bash --version') do
     it { should return_stdout(/4\.3/) }
   end
 end
 
-context 'git' do
-  describe command('which git') do
-    it { should return_stdout('/bin/git') }
-  end
-
+describe 'git' do
   describe command('/usr/local/bin/git --version') do
     it { should return_stdout(/1\.9\.0/) }
   end
