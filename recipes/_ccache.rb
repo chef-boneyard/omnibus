df178c3f454c2fc9190f82b86a0dee6340a403d1
Seth Vargo <sethvargo@gmail.com>
2014-05-05 13:03:29 -0400

    fix things

--- recipes/_ccache.rb
+++ recipes/_ccache.rb
@@ -34,7 +34,6 @@ end
 
 %w(gcc g++ cc c++).each do |compiler|
   link "/usr/local/bin/#{compiler}" do
-    to "/usr/local/bin/ccache"
-    # This is a really long line. # This is a really long line. # This is a really long line. # This is a really long line. # This is a really long line.
+    to '/usr/local/bin/ccache'
   end
 end
