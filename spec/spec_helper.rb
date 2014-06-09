2162f45cc8742c0f079b95970a66e69d059e3707
Julian C. Dunn <jdunn@getchef.com>
2014-06-09 11:36:10 -0400

    config.treat_symbols_as_metadata_keys_with_true_values is the default under RSpec 3, and setting it to false has no effect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ RSpec.configure do |config|
   config.log_level = :fatal
 
   # Why aren't these the defaults?
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
