bee47ddbc8f96dc4e7b6c16922480d69047d05cb
Seth Chisamore <schisamo@getchef.com>
2014-10-22 17:02:06 -0400

    Fix `_openssl` recipe spec

--- spec/recipes/openssl_spec.rb
+++ spec/recipes/openssl_spec.rb
@@ -14,17 +14,6 @@ describe 'omnibus::_openssl' do
     end
   end
 
-  context 'on freebsd' do
-    let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'freebsd', version: '9.1')
-        .converge(described_recipe)
-    end
-
-    it 'installs the correct development packages' do
-      expect(chef_run).to install_package('security/openssl')
-    end
-  end
-
   context 'on mac_os_x' do
     let(:chef_run) do
       ChefSpec::ServerRunner.new(platform: 'mac_os_x', version: '10.8.2')
