2621e2e2b3b4ebdb6f37350216178c69418f2aa8
kaustubh-d <kaustubh@clogeny.com>
2015-03-25 12:11:39 +0530

    Updates to support RHEL/Ubuntu ppc64/le

--- libraries/remote_install.rb
+++ libraries/remote_install.rb
@@ -25,6 +25,8 @@ class Chef
     default_action :install
 
     attribute :source,            kind_of: String, required: true
+    attribute :source_type,       kind_of: String, default: 'gzip', equal_to: %w(tar gzip bz2)
+    attribute :relative_path,     kind_of: String
     attribute :version,           kind_of: String, required: true
     attribute :checksum,          kind_of: String, required: true
     attribute :build_command,     kind_of: String
@@ -77,20 +79,38 @@ EOH
 
     def tarball_name
       @tarball_name ||= begin
-        if (tarball_extension = new_resource.source.match(/\.tgz|\.tar\.gz$/))
+        if (tarball_extension = new_resource.source.match(/\.tar\.bz2|\.tgz|\.tar\.gz|\.tar$/))
+          @tarball_extension = tarball_extension.to_s[1..-1]
           ::File.basename(new_resource.source, tarball_extension.to_s)
         else
+          @tarball_extension = default_extension
           id
         end
       end
     end
 
+    def default_extension
+      case new_resource.source_type
+      when 'tar'
+        'tar'
+      when 'gzip'
+        'tar.gz'
+      when 'bz2'
+        'tar.bz2'
+      end
+    end
+
+    def tarball_extension
+      tarball_name
+      @tarball_extension
+    end
+
     def cache_path
-      @cache_path ||= ::File.join(Config[:file_cache_path], "#{id}.tar.gz")
+      @cache_path ||= ::File.join(Config[:file_cache_path], "#{id}.#{tarball_extension}")
     end
 
     def extract_path
-      @extract_path ||= ::File.join(Config[:file_cache_path], tarball_name)
+      @extract_path ||= ::File.join(Config[:file_cache_path], new_resource.relative_path || tarball_name)
     end
 
     def download
@@ -109,9 +129,20 @@ EOH
       raise ChecksumVerificationFailure.new(new_resource, checksum) unless new_resource.checksum == checksum
     end
 
+    def extract_command
+      case new_resource.source_type
+      when 'tar'
+        'tar -xvf'
+      when 'gzip'
+        'tar -zxvf'
+      when 'bz2'
+        'tar -jxvf'
+      end
+    end
+
     def extract
       execute = Resource::Execute.new(label('extract'), run_context)
-      execute.command("tar -xzvf #{id}.tar.gz")
+      execute.command("#{extract_command} #{cache_path}")
       execute.cwd(Config[:file_cache_path])
       execute.run_action(:run)
     end
---
+++ recipes/_libffi.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook Name:: omnibus
+# Recipe:: _libffi
+#
+# Copyright 2015, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# We need libffi 3.2.1 on ppc64/ppc64le systems in order
+# for ruby ffi gem to get installed. Someday when ruby ffi
+# updates embedded libffi 3.2.1 and fixes its extension
+# build process to consume embedded libffi properly this wont
+# be required. https://github.com/ffi/ffi/pull/424#issuecomment-83935000
+
+# Include the common recipe
+include_recipe 'omnibus::_common'
+
+include_recipe 'omnibus::_compile'
+
+chef_gem 'pkg-config'
+require 'pkg-config'
+
+# libffi.pc is installed at below path and its not default
+# search paths for pkg-config
+build_env = {}
+build_env['PKG_CONFIG_PATH'] = '/usr/local/lib/pkgconfig/'
+
+remote_install 'libffi' do
+  # source code is maintained on debian ftp
+  source 'ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz'
+  version '3.2.1'
+  checksum 'd06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37'
+  environment build_env
+  build_command './configure'
+  compile_command "make -j #{node.builders}"
+  install_command 'make install'
+  not_if { ::PKGConfig.check_version?('libffi', 3, 2, 1) }
+end
