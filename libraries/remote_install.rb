375ae45b646c07f94cf8bc03db2faa259a0fbe79
kaustubh-d <kaustubh@clogeny.com>
2015-03-30 21:15:48 -0700

    typo

--- libraries/remote_install.rb
+++ libraries/remote_install.rb
@@ -25,7 +25,7 @@ class Chef
     default_action :install
 
     attribute :source,            kind_of: String, required: true
-    attribute :source_type,       kind_of: String, default: 'gzip', equal_to: %w(tar, gzip, bz2)
+    attribute :source_type,       kind_of: String, default: 'gzip', equal_to: %w(tar gzip bz2)
     attribute :relative_path,     kind_of: String
     attribute :version,           kind_of: String, required: true
     attribute :checksum,          kind_of: String, required: true
