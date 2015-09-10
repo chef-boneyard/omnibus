3d1092355af9550055d16be484cb831fc1f955c2
Tim Smith <tsmith84@gmail.com>
2015-09-09 20:20:00 -0700

    Add supermarket metadata and additional platforms

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,19 @@
 name              'omnibus'
 maintainer        'Chef Software, Inc.'
-maintainer_email  'releng@getchef.com'
+maintainer_email  'releng@chef.io'
 license           'Apache 2.0'
 description       'Prepares a machine to be an Omnibus builder.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.6.13'
 
+supports 'amazon'
 supports 'centos'
 supports 'debian'
 supports 'fedora'
+supports 'oracle'
+supports 'redhat'
 supports 'freebsd'
+supports 'scientific'
 supports 'mac_os_x'
 supports 'suse'
 supports 'ubuntu'
@@ -23,3 +27,6 @@ depends 'homebrew',        '~> 1.9'
 depends 'windows',         '~> 1.30'
 depends 'wix',             '~> 1.1'
 depends 'windows-sdk',     '~> 1.0'
+
+source_url 'https://github.com/chef-cookbooks/omnibus' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/omnibus/issues' if respond_to?(:issues_url)
