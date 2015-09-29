eb5cd10224d26e5679a22d6e38e131b28742ebf1
Seth Chisamore <schisamo@chef.io>
2015-09-29 19:43:16 -0400

    Ensure Chef Sugar is installed for unit tests

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 version '0.1.0'
 
+depends 'chef-sugar'
 depends 'delivery-truck'
+depends 'fancy_execute'
--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -3,4 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# Ensure chef-sugar is installed for ChefSpec tests
+include_recipe 'chef-sugar::default'
+
 include_recipe 'delivery-truck::unit'
