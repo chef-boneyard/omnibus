af55c5220c9e0d12091773aee38e61ab3f6031d8
Seth Chisamore <schisamo@chef.io>
2015-09-30 08:59:17 -0400

    delivery-truck already has the proper dep on chef-sugar

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -4,7 +4,4 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-# Ensure chef-sugar is installed for ChefSpec tests
-include_recipe 'chef-sugar::default'
-
 include_recipe 'delivery-truck::unit'
