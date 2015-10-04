452b3a2f835f5b0c6fa22fd3077a6bba1ef41ea6
Seth Chisamore <schisamo@chef.io>
2015-10-04 13:29:05 -0400

    Publish to supermarket.chef.io during `delivered/deploy`
    Per some internal discussion, we should publish a cookbook to an internal
    Supermarket instance at `build/publish` and publish to a public Supermarket
    instance when things are considered ‘delivered’!
    
    This change is an experiment to test that theory, eventually this code
    should become part of delivery-truck proper (as a `delivery_supermarket`
    resource).

--- .delivery/build-cookbook/recipes/default.rb
+++ .delivery/build-cookbook/recipes/default.rb
@@ -4,3 +4,10 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::default'
+
+# Needed for cookbook publishing during `delivered/deploy`
+chef_gem 'knife-supermarket' do
+  compile_time false
+  only_if { delivery_environment == 'delivered' }
+  action :install
+end
