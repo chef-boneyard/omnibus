99ff4ab0fd8cf7b31320494fb02c3f9bfc444914
Tim Smith <tsmith@chef.io>
2016-12-08 13:50:16 -0800

    Actually require 12.5.1 and fix test runs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -33,4 +33,4 @@ depends 'windows-sdk'
 
 source_url 'https://github.com/chef-cookbooks/omnibus'
 issues_url 'https://github.com/chef-cookbooks/omnibus/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.5.1' if respond_to?(:chef_version)
