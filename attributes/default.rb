7b9fda60eb21809c4be9d48e06a975edd6e52eeb
Scott Hain <shain@chef.io>
2018-04-03 09:21:50 -0700

    Always pull the latest toolchain from stable
    Signed-off-by: Scott Hain <shain@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,7 @@ default['omnibus'].tap do |omnibus|
   omnibus['build_user_home']    = nil
   omnibus['install_dir']        = nil
   omnibus['toolchain_name']     = 'omnibus-toolchain'
-  omnibus['toolchain_version']  = '1.1.82'
+  omnibus['toolchain_version']  = 'latest'
   omnibus['toolchain_channel']  = 'stable'
 
   if platform_family?('windows')
