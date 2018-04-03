fb7e79c5ca65c96ee4e0a9cbbcf26d5f425d5208
Scott Hain <shain@chef.io>
2018-04-03 08:59:40 -0700

    Remove unused attributes
    Signed-off-by: Scott Hain <shain@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,8 +22,6 @@ default['omnibus'].tap do |omnibus|
   omnibus['toolchain_name']     = 'omnibus-toolchain'
   omnibus['toolchain_version']  = '1.1.82'
   omnibus['toolchain_channel']  = 'stable'
-  omnibus['git_version']        = '2.6.2'
-  omnibus['ruby_version']       = '2.1.8'
 
   if platform_family?('windows')
     omnibus['build_user_group'] = 'Administrators'
